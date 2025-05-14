ffba29bbd4f70360fc6147a534ddec38f95eab18
Dương Trí Dũng <crytv778@gmail.com>
2025-05-14 10:28:18 +0700

    Refactor keep_alive.py to use waitress for serving and add it to requirements.txt

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+from waitress import serve  # <- thêm dòng này
 
 app = Flask('')
 
@@ -8,7 +9,7 @@ def home():
     return "Hello. I am alive!", 200
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    serve(app, host="0.0.0.0", port=8080)  # <- dùng waitress thay vì app.run()
 
 def keep_alive():
     t = Thread(target=run)
